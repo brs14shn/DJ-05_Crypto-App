02bcdd8846428ce939d216ffe02c668b676d903b
brs14shn <brsshn292929@gmail.com>
2022-09-17 15:43:44 +0300

    home page

--- app/views.py
+++ app/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+import requests # İstek gönderiyoruz =>axios 
 
 # Create your views here.
 
-def home(requeset):
-    pass
+def home(request):
+     url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+     response = requests.get(url)
+     content = response.json()
+     print(content)
+
+     return render(request,"app/home.html")
+     
